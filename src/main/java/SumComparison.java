301325cff6d72a2742c96bdc8e511e7c5c367385
Andrii Kovsh <kobannn@gmail.com>
2023-06-15 22:11:42 +0300

    Adding code with working homework

--- src/main/java/SumComparison.java
+++ src/main/java/SumComparison.java
@@ -1,21 +1,23 @@
 public class SumComparison {
     public static void main(String[] args) {
         int value1 = 15;
-        int value2 = 4;
-        int value3 = 7;
-        int value4 = 8;
+        int value2 = 3;
+        int value3 = 8;
+        int value4 = 12;
 
         int first_sum = value1 + value2;
         int second_sum = value3 + value4;
 
-        boolean FisrtSumIsGreater = first_sum > second_sum;
-        System.out.println("Перша сума більша:" + FisrtSumIsGreater);
+        boolean FirstSumIsGreater = first_sum > second_sum;
+        System.out.println("Перша сума більша: " + FirstSumIsGreater);
 
         first_sum += 1;
         second_sum -= 2;
 
-        System.out.println(first_sum);
-        System.out.println(second_sum);
+        boolean UpdatedFirstSumIsGreater = first_sum > second_sum;
+        System.out.println("Перша сума після збільшення більша: " + UpdatedFirstSumIsGreater);
 
+        boolean AnySumIsEven = first_sum % 2 == 0 || second_sum % 2 == 0;
+        System.out.println("Принаймні одна сума кратна 2: " + AnySumIsEven);
     }
 }
